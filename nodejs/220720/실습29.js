53e5e680162bd9ab162245ffaf6db4568be3c901
Mokdasoo <dgstudylife@naver.com>
2022-07-20 18:42:57 +0900

    실습28 도전

--- nodejs/220720/실습29.js
+++ nodejs/220720/실습29.js
@@ -2,5 +2,17 @@ const fs = require("fs").promises;
 const http = require("http");
 
 const server = http.createServer(async function(req,res){
-
+    try{
+        const data = await fs.readFile("../../week 1/220629/실습6.html");
+        res.writeHead(200);
+        res.end(data);
+    } catch(err){
+        console.error(err);
+        res.writeHead(404);
+        res.end(err.message);
+    }
 });
+
+server.listen(8080, function(){
+    console.log("8080번 포트");
+})
