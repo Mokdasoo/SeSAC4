e17aedfa3af99643f90fe3c04de9778fe6ac9f60
Mokdasoo <dgstudylife@naver.com>
2022-07-22 18:21:16 +0900

    mini project start

--- nodejs/portpolio/index.js
+++ nodejs/portpolio/index.js
@@ -3,15 +3,20 @@ const app = express();
 const port = 8080;
 
 
-app.use('/abc', express.static('public'));
+app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
-    var list = ['apple', 'banana'];
-    res.render("실습6", {list:list});
+    res.render("index");
+});
+app.get('/color', (req,res) => {
+    res.render("ex16");
 });
 app.listen(port, () => {
     console.log('Server port : ', port);
+});
+app.get('/**', (req,res) => {
+    console.log(req.url);
 });
 //express Method =>
 //get = 요청 받을 때, 쿼리 주소에 보이는 거
