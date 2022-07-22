5b39add4c7eaba7c2845c0fe2370d5290307f491
Mokdasoo <dgstudylife@naver.com>
2022-07-22 14:17:33 +0900

    a

--- nodejs/220720/실습28_callback.js
+++ nodejs/220720/실습28_callback.js
@@ -1,54 +1,35 @@
 const fs = require("fs");
 const http = require("http");
 
-
-
-
-
-const server = http.createServer(function(req, res){
-
-    try{
-        console.log(req.url);
-
-        if(req.url === '/'){
-            var filePath = "./../../week 1/220629/실습6.html";
-            var getdata = function(filePath, callback){
-                fs.readFile(filePath, function(err, data){
-                    if(err){
-                        callback(err, null);
-                    }else{
-                        callback(null, data);
-                    }
-                });
-            }
+const server = http.createServer( function(req, res){
+    fs.readFile("./실습6.html", function(err, data){
+        if(err){
+            //promise 에서의 catch
+            console.error(err);
+            res.writeHead(404);
+            res.end(err.message);
+        }else{
+            //promise 에서의 try
+            console.log(req.url);
             res.writeHead(200);
-            res.end(getdata);
+            res.end(data);
         }
-        if(req.url === '/grass.png'){
-            filePath = './../../week 1/220629/grass.png';
-            getdata = function(filePath, callback){
-                fs.readFile(filePath, function(err, data){
-                    if(err){
-                        callback(err, null);
-                    }else{
-                        callback(null, data);
-                    }
-                });
-            }
-            console.log('picture loading...');
-            res.writeHead(200);
-            res.write(getdata);
-            res.end();
-        }
-    } catch(err){
-        console.error(err);
-        res.writeHead(404);
-        res.end(err.message);
-    }
+    });
+    // try{
+    //     console.log(req.url);
+    //     const data = fs.readFile("./실습6.html");
+    //     res.writeHead(200);
+    //     res.end(data);
+    // } catch(err){
+    //     console.error(err);
+    //     res.writeHead(404);
+    //     res.end(err.message);
+    // }
+    
 });
 
-server.listen(8080, function(){
-    console.log("8080번 포트");
+server.listen(8000, function(){
+    console.log("8000번 포트");
 })
 
 server.on( "request", function(){
@@ -60,9 +41,3 @@ server.on("connection", function(){
 server.on("checkContinue", function(){
     console.log( "Client checkContinue");
 })
-
-
-
-
-
-
